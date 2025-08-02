025612441a4e0c2df4e494e2af61b749851930ca
Nguyen Du <nguyendu2004.anhuu@gmail.com>
2025-08-02 13:14:44 +0700

    debai

---
+++ OOP_CK/Đề-bài.cpp
@@ -0,0 +1,5 @@
+Đề bài Mô Phỏng Sát thương nhân vật
++ Lớp cơ sở: VuKhi (Tên vũ khí, Sát thương cơ bản, Tốc độ ra đòn).
++ Lớp kế thừa: Sung (Số lượng đạn trong ống, Tốc độ thay băng), Kiem (Độ bền), PhepThuat (Loại phép, Mana tiêu thụ trên giây).
++ Đa hình: Phương thức TanCong() (mô tả cách tấn công). Phương thức SatThuong(int t) sau t giây nếu sử dụng vũ khí này tấn công.
+ 
